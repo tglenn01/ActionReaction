d47dd3feb58e12651bb0696d32a76cadf952b073
tjg60 <tjg604@gmail.com>
2020-03-20 22:26:36 -0700

    Added documentation to main, final commit of phase 3

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -8,11 +8,13 @@ import ui.interfaces.MainScreenInterface;
 // launches the primary Stage which is then passed on into a new GameUI
 public class Main extends Application {
 
+    // Launches the application
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
+    // EFFECTS: starts the app, sets primary stage to a new GameUI then runs new MainScreenInterface
     public void start(Stage primaryStage) {
         GameUI.getInstance().setPrimaryStage(primaryStage);
         primaryStage.setTitle("Action Reaction!");
